4cae351926cc56e91cc7281e5c5f76f4a38ca61d
Brian Heise <bnheise@gmail.com>
2021-06-14 21:53:06 +0900

    configured mongoose connection and environment variables

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_PASS = process.env.DB_PASS;
+const DB_USER = process.env.DB_USER;
+const DB_IP = process.env.DB_IP || "mongo";
+const mongoUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_IP}:${DB_PORT}/?authSource=admin`;
 
-app.get('/', (req, res) => {
-  res.send('<h2>Hi There</h2>');
+if (!DB_PASS) {
+  console.error("No value for DB_PASS found in .env file");
+}
+
+if (!DB_USER) {
+  console.error("No value for DB_USER found in .env file");
+}
+
+(async () => {
+  try {
+    const mongoDb = await mongoose.connect(mongoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.info("Logged into mongoDb");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hi There</h2>");
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.info(`listening on port ${port}`));
+app.listen(PORT, () => console.info(`listening on port ${PORT}`));
