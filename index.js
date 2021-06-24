f77e038e2c59c8e4cb1f377798d5cd17af365a39
Brian Heise <bnheise@gmail.com>
2021-06-24 21:14:00 +0900

    implemented redis

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import postRouter from "./routes/post.js";
 import userRouter from "./routes/user.js";
+import session from "express-session";
+import redis from "redis";
+import RedisStore from "connect-redis";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +13,11 @@ const DB_PASS = process.env.DB_PASS;
 const DB_USER = process.env.DB_USER;
 const DB_IP = process.env.DB_IP || "mongo";
 const mongoUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_IP}:${DB_PORT}/?authSource=admin`;
+let store = RedisStore(session);
+let redisClient = redis.createClient({
+  host: process.env.REDIS_URL || "redis",
+  port: process.env.REDIS_PORT || 6379,
+});
 
 if (!DB_PASS) {
   console.error("No value for DB_PASS found in .env file");
@@ -31,6 +39,19 @@ if (!DB_USER) {
   }
 })();
 
+app.use(
+  session({
+    store: new store({ client: redisClient }),
+    secret: process.env.SESSION_SECRET || "asdfghjkl",
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 300000,
+    },
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
