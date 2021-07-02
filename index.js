a73e317c519161a7d72a780dfafeae44d79b1470
Brian Heise <bnheise@gmail.com>
2021-07-02 17:41:56 +0900

    reset

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const DB_PASS = process.env.DB_PASS;
 const DB_USER = process.env.DB_USER;
 const DB_IP = process.env.DB_IP || "mongo";
 const mongoUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_IP}:${DB_PORT}/?authSource=admin`;
-console.log(DB_USER, DB_PASS);
+
 let store = RedisStore(session);
 let redisClient = redis.createClient({
   host: process.env.REDIS_URL || "redis",
@@ -31,45 +31,43 @@ if (!DB_USER) {
 }
 
 (async () => {
-  console.log(mongoUrl);
   try {
     await mongoose.connect(mongoUrl, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
     console.info("Logged into mongoDb");
-
-    app.enable("trust proxy");
-    app.use(cors({}));
-
-    app.use(
-      session({
-        store: new store({ client: redisClient }),
-        secret: process.env.SESSION_SECRET || "asdfghjkl",
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-          secure: false,
-          resave: false,
-          saveUninitialized: false,
-          httpOnly: true,
-          maxAge: 300000,
-        },
-      })
-    );
-    app.use(express.json());
-
-    app.get("/api", (req, res) => {
-      res.send("<h2>I love hamburgers!!!.</h2>");
-      console.log("lalal");
-    });
-
-    app.use("/api/v1/posts", postRouter);
-    app.use("/api/v1/users", userRouter);
-
-    app.listen(PORT, () => console.info(`listening on port ${PORT}`));
   } catch (error) {
-    console.log("UUURRL!", mongoUrl);
     console.error(error);
   }
 })();
+
+app.enable("trust proxy");
+app.use(cors({}));
+
+app.use(
+  session({
+    store: new store({ client: redisClient }),
+    secret: process.env.SESSION_SECRET || "asdfghjkl",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 300000,
+    },
+  })
+);
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.send("<h2>I love hamburgers!!!.</h2>");
+  console.log("lalal");
+});
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
+
+app.listen(PORT, () => console.info(`listening on port ${PORT}`));
