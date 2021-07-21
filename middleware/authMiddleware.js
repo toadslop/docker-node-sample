74b25a661900f87f6228b31b48efe1fd7a543363
Brian Heise <bnheise@gmail.com>
2021-07-22 08:57:53 +0900

    disabled eslint no expression for next() function

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,8 +2,9 @@ export const protect = (req, res, next) => {
   const { user } = req.session;
 
   if (!user) {
-    return res.status(401).json({ stuatus: "fail", message: "unauthorized" });
+    return res.status(401).json({ status: "fail", message: "unauthorized" });
   }
 
+  // eslint-disable-next-line functional/no-expression-statement
   next();
 };
