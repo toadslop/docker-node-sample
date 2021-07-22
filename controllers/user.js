08be552618822901926902a0e8bd3bc6cb6d0efe
Brian Heise <bnheise@gmail.com>
2021-07-22 09:00:58 +0900

    replaced login function with functional version

--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,19 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import { createEntity, tryCrud } from "./controllerUtils/index.js";
+import { lensPath, set } from "ramda";
+
+const sessionUserLens = lensPath(["session", "user"]);
+
+const createUser = createEntity(User);
+const tryCreateUser = tryCrud(createUser);
 
 const signUp = async (req, res) => {
   const { username, userpass } = req.body;
-  try {
-    const hashpassword = await bcrypt.hash(userpass, 12);
-    const newUser = await User.create({ username, userpass: hashpassword });
-    req.session.user = newUser;
-    res.status(201).json({
-      status: "sucess",
-      data: {
-        user: newUser,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({
-      status: "fail",
-    });
-  }
+  const hashpassword = await bcrypt.hash(userpass, 12);
+  const result = await tryCreateUser({ username, userpass: hashpassword });
+  const reqWithUser = set(sessionUserLens)(result.data)(req);
+  return { res: res.status(result.code).json(result), req: reqWithUser };
 };
 
 const login = async (req, res) => {
@@ -29,7 +24,7 @@ const login = async (req, res) => {
     if (!currentUser) {
       return res.status(404).json({
         status: "fail",
-        message: "user not found",
+        message: "user not found"
       });
     }
 
@@ -38,18 +33,18 @@ const login = async (req, res) => {
     if (isCorrect) {
       req.session.user = currentUser;
       res.status(200).json({
-        status: "success",
+        status: "success"
       });
     } else {
       res.status(400).json({
         status: "fail",
-        message: "Incorrect username or password.",
+        message: "Incorrect username or password."
       });
     }
   } catch (error) {
     console.error(error);
     res.status(400).json({
-      status: "fail",
+      status: "fail"
     });
   }
 };
