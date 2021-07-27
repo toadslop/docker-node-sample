3ea57711c1dbd996892ae94766ef510133b07c5a
Brian Heise <bnheise@gmail.com>
2021-07-27 21:56:57 +0900

    functionalized login

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,35 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
-import { createEntity, tryCrud } from "./controllerUtils/index.js";
-import { lensPath, set } from "ramda";
+import { createEntity, findOne, tryCrud } from "./controllerUtils/index.js";
+import { UNAUTHORIZED } from "./controllerUtils/responses.js";
+import { lensPath, set, omit } from "ramda";
 
 const sessionUserLens = lensPath(["session", "user"]);
 
 const createUser = createEntity(User);
+const findOneUser = findOne(User);
+
 const tryCreateUser = tryCrud(createUser);
+
+const loginSuccess = (req) => (res) => (result) => {
+  const reqWithUser = set(sessionUserLens)(result.data)(req);
+  return {
+    res: res.status(result.code).json(result),
+    req: reqWithUser
+  };
+};
+
+const loginFail = (res) => (req) => {
+  const result = UNAUTHORIZED();
+  return { res: res.status(result.code).json(result), req };
+};
+
+const checkCredentials = (req) => (res) => (result) =>
+  bcrypt.compare(req.body.userpass, result.data.userpass)
+    ? loginSuccess(req)(res)(result)
+    : loginFail(res)(req);
+
+const tryFindOneUser = tryCrud(findOneUser);
 
 const signUp = async (req, res) => {
   const { username, userpass } = req.body;
@@ -17,36 +40,9 @@ const signUp = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { username, userpass } = req.body;
-  try {
-    const currentUser = await User.findOne({ username });
-
-    if (!currentUser) {
-      return res.status(404).json({
-        status: "fail",
-        message: "user not found"
-      });
-    }
-
-    const isCorrect = bcrypt.compare(userpass, currentUser.userpass);
-
-    if (isCorrect) {
-      req.session.user = currentUser;
-      res.status(200).json({
-        status: "success"
-      });
-    } else {
-      res.status(400).json({
-        status: "fail",
-        message: "Incorrect username or password."
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({
-      status: "fail"
-    });
-  }
+  const { username } = req.body;
+  const result = await tryFindOneUser({ username: username });
+  return checkCredentials(req)(res)(result);
 };
 
 const user = { signUp, login };
